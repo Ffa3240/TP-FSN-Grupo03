effc90a6c7d8920b611d804e86242b2ace150ef6
Francisca Mascia <franmas22@gmail.com>
2023-08-25 00:02:01 -0300

    Adding all the elements to login form and fixing image path in template.js

--- js/template.js
+++ js/template.js
@@ -3,13 +3,13 @@ var wHeader = document.getElementById("idHeader")
 wHeader.innerHTML = `
 <nav>
 <picture>
-    <a href="index.html"><img src="./img/branding/logo_light_horizontal.svg" alt="isologotipo de la marca FunkpoShop"></a>
+    <a href="index.html"><img src="../img/branding/logo_light_horizontal.svg" alt="isologotipo de la marca FunkpoShop"></a>
 </picture>
 <ul>
-    <li><a href="./pages/shop.html">SHOP</a></li>
-    <li><a href="./pages/contacto.html">CONTACTO</a></li>
-    <li><a href="./pages/login.html">LOGIN</a></li>
-    <li><a href="./pages/carrito.html"><img src="./img/icons/cart-icon.svg" alt="icono de carrito" /></a></li>
+    <li><a href="shop.html">SHOP</a></li>
+    <li><a href="contacto.html">CONTACTO</a></li>
+    <li><a href="login.html">LOGIN</a></li>
+    <li><a href="carrito.html"><img src="../img/icons/cart-icon.svg" alt="icono de carrito" /></a></li>
 </ul>
 </nav>
 `
@@ -18,14 +18,14 @@ wFooter.innerHTML = `
 <section>
             <nav>
                 <ul>
-                    <li><a href="./pages/shop.html">SHOP</a></li>
-                    <li><a href="./pages/register.html">REGISTRARSE</a></li>
-                    <li><a href="./pages/login.html">LOGIN</a></li>
-                    <li><a href="./pages/contactos.html">CONTACTO</a></li>
+                    <li><a href="shop.html">SHOP</a></li>
+                    <li><a href="register.html">REGISTRARSE</a></li>
+                    <li><a href="login.html">LOGIN</a></li>
+                    <li><a href="contacto.html">CONTACTO</a></li>
                 </ul>
 
                 <picture>
-                    <img src="./img/branding/isotype.svg" alt="Isotipo de la marca Funkoshop">
+                    <img src="../img/branding/isotype.svg" alt="Isotipo de la marca Funkoshop">
                 </picture>
             </nav>
         </section>
