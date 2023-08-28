3e2c1ed8697c8b5419590d736dde0885d8c43e18
RamirezAgus <rlagustin0@gmail.com>
2023-08-27 22:20:07 -0300

    changes: add changes in routes images and change the structure of item.html

--- js/template.js
+++ js/template.js
@@ -3,13 +3,13 @@ var wHeader = document.getElementById("idHeader")
 wHeader.innerHTML = `
 <nav>
 <picture>
-    <a href="index.html"><img src="./img/branding/logo_light_horizontal.svg" alt="isologotipo de la marca FunkpoShop"></a>
+    <a href="index.html"><img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/logo_light_horizontal_bx6kzi.svg" alt="isologotipo de la marca FunkpoShop"></a>
 </picture>
 <ul>
     <li><a href="./pages/shop.html">SHOP</a></li>
     <li><a href="./pages/contacto.html">CONTACTO</a></li>
     <li><a href="./pages/login.html">LOGIN</a></li>
-    <li><a href="./pages/carrito.html"><img src="./img/icons/cart-icon.svg" alt="icono de carrito" /></a></li>
+    <li><a href="./pages/carrito.html"><img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183785/Codo%20a%20Codo/icons/cart-icon_dgq3n7.svg" alt="icono de carrito" /></a></li>
 </ul>
 </nav>
 `
@@ -25,7 +25,7 @@ wFooter.innerHTML = `
                 </ul>
 
                 <picture>
-                    <img src="./img/branding/isotype.svg" alt="Isotipo de la marca Funkoshop">
+                    <img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/isotype_zybtmc.svg" alt="Isotipo de la marca Funkoshop">
                 </picture>
             </nav>
         </section>
