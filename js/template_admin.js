56b1f212d6187283d9c6f6fca7c5eec4f7400759
mcastellarin <mcastellarin@outlook.com.ar>
2023-08-28 21:28:34 -0300

    add register.html

---
+++ js/template_admin.js
@@ -0,0 +1,31 @@
+
+var wHeader = document.getElementById("idHeader")
+wHeader.innerHTML = `
+<nav>
+    <picture>
+        <a href="index.html"><img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/logo_light_horizontal_bx6kzi.svg" alt="isologotipo de la marca FunkpoShop"></a>
+    </picture>
+    <ul>
+        <li><a href="../index.html">VER TIENDA</a></li>
+        <li><a href="../pages/admin.html">ADMIN</a></li>
+        <li><a href="../pages/logout.html">SALIR</a></li>
+    </ul>
+</nav>
+`
+var wFooter = document.getElementById("idFooter")
+wFooter.innerHTML = `
+<section>
+    <nav>
+        <ul>
+            <li><a href="../pages/shop.html">SHOP</a></li>
+            <li><a href="../pages/register.html">REGISTRARSE</a></li>
+            <li><a href="../pages/login.html">INGRESAR</a></li>
+            <li><a href="../pages/contact.html">CONTACTO</a></li>
+        </ul>
+        <picture>
+            <img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/isotype_zybtmc.svg" alt="Isotipo de la marca Funkoshop">
+        </picture>
+    </nav>
+</section>
+<p>All rights reserved 2023 - Funkoshop &copy;</p>
+`
