8d21a11b52c086094bf9817c1e003ed6bfa36971
mcastellarin <mcastellarin@outlook.com.ar>
2023-09-27 19:00:42 -0300

    Several adjustments

--- js/template_admin.js
+++ js/template_admin.js
@@ -1,31 +1,36 @@
 
 var wHeader = document.getElementById("idHeader")
 wHeader.innerHTML = `
-<nav>
-    <picture>
-        <a href="index.html"><img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/logo_light_horizontal_bx6kzi.svg" alt="isologotipo de la marca FunkpoShop"></a>
+<nav class="navbar container">
+    <picture class="nabvar__logo">
+        <a href="/TP-FSN-Grupo03/index.html"><img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/logo_light_horizontal_bx6kzi.svg" alt="isologotipo de la marca FunkpoShop"></a>
     </picture>
-    <ul>
-        <li><a href="../index.html">VER TIENDA</a></li>
-        <li><a href="../pages/admin.html">ADMIN</a></li>
-        <li><a href="../pages/logout.html">SALIR</a></li>
+    <ul class="navbar__menu">
+        <li><a class="navbar__link" href="/TP-FSN-Grupo03/index.html">VER TIENDA</a></li>
+        <li><a class="navbar__link" href="/TP-FSN-Grupo03/pages/admin/admin.html">ADMIN</a></li>
+        <li><a class="navbar__link" href="">SALIR</a></li>
     </ul>
 </nav>
 `
 var wFooter = document.getElementById("idFooter")
 wFooter.innerHTML = `
 <section>
-    <nav>
-        <ul>
-            <li><a href="../pages/shop.html">SHOP</a></li>
-            <li><a href="../pages/register.html">REGISTRARSE</a></li>
-            <li><a href="../pages/login.html">INGRESAR</a></li>
-            <li><a href="../pages/contact.html">CONTACTO</a></li>
+    <nav class="navbar container">
+        <ul class="navbar__item">
+            <li class="navbar__item">
+                <a class="navbar__link" href="/TP-FSN-Grupo03/pages/shop.html">SHOP</a></li>
+            <li class="navbar__item">
+                <a class="navbar__link" href="/TP-FSN-Grupo03/pages/admin/register.html">REGISTRARSE</a></li>
+            <li class="navbar__item">
+                <a class="navbar__link" href="/TP-FSN-Grupo03/pages/admin/login.html">LOGIN</a></li>
+            <li class="navbar__item">
+                <a class="navbar__link" href="/TP-FSN-Grupo03/pages/contact.html">CONTACTO</a></li>
         </ul>
+
         <picture>
             <img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/isotype_zybtmc.svg" alt="Isotipo de la marca Funkoshop">
         </picture>
     </nav>
 </section>
-<p>All rights reserved 2023 - Funkoshop &copy;</p>
+<p class="footer__copy">All rights reserved 2023 - Funkoshop &copy;</p>
 `
