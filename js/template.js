2ae880974561ca35c4885bd3d9aa87bcc4616907
RamirezAgus <rlagustin0@gmail.com>
2023-10-25 09:47:42 -0300

    Correciones en las rutas relativas

--- js/template.js
+++ js/template.js
@@ -3,11 +3,11 @@ var wHeader = document.getElementById("idHeader")
 wHeader.innerHTML = `
 <nav class="navbar container">
 <picture class="nabvar__logo">
-    <a href="/TP-FSN-Grupo03/index.html"><img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/logo_light_horizontal_bx6kzi.svg" alt="isologotipo de la marca FunkpoShop"></a>
+    <a href="/index.html"><img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/logo_light_horizontal_bx6kzi.svg" alt="isologotipo de la marca FunkpoShop"></a>
 </picture>
 <ul class="navbar__menu">
     <li class="navbar__item with-submenu">
-        <a class="navbar__link with-icon" href="/TP-FSN-Grupo03/pages/shop.html">SHOP
+        <a class="navbar__link with-icon" href="./pages/shop.html">SHOP
             <iconify-icon icon="tabler:chevron-down" style="color: white;"></iconify-icon>
         </a>
         <ul class="submenu">
@@ -23,10 +23,10 @@ wHeader.innerHTML = `
     </ul>
 
     </li>
-    <li><a class="navbar__link" href="/TP-FSN-Grupo03/pages/contact.html">CONTACTO</a></li>
-    <li><a class="navbar__link" href="/TP-FSN-Grupo03/pages/admin/login.html">LOGIN</a></li>
+    <li><a class="navbar__link" href="/pages/contact.html">CONTACTO</a></li>
+    <li><a class="navbar__link" href="/pages/login.html">LOGIN</a></li>
     <li class="navbar__item">
-        <a class="navbar__cart" href="/TP-FSN-Grupo03/pages/carrito.html">
+        <a class="navbar__cart" href="/pages/carrito.html">
             <img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183785/Codo%20a%20Codo/icons/cart-icon_dgq3n7.svg" alt="icono de carrito" />
         </a>
     </li>
--- js/template_admin.js
+++ js/template_admin.js
@@ -3,11 +3,11 @@ var wHeader = document.getElementById("idHeader")
 wHeader.innerHTML = `
 <nav class="navbar container">
     <picture class="nabvar__logo">
-        <a href="/TP-FSN-Grupo03/index.html"><img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/logo_light_horizontal_bx6kzi.svg" alt="isologotipo de la marca FunkpoShop"></a>
+        <a href="/index.html"><img src="https://res.cloudinary.com/db6uriqjm/image/upload/v1693183561/Codo%20a%20Codo/branding/logo_light_horizontal_bx6kzi.svg" alt="isologotipo de la marca FunkpoShop"></a>
     </picture>
     <ul class="navbar__menu">
-        <li><a class="navbar__link" href="/TP-FSN-Grupo03/index.html">VER TIENDA</a></li>
-        <li><a class="navbar__link" href="/TP-FSN-Grupo03/pages/admin/admin.html">ADMIN</a></li>
+        <li><a class="navbar__link" href="/pages/shop.html">VER TIENDA</a></li>
+        <li><a class="navbar__link" href="/pages/Admin/admin.html">ADMIN</a></li>
         <li><a class="navbar__link" href="">SALIR</a></li>
     </ul>
 </nav>
@@ -18,13 +18,13 @@ wFooter.innerHTML = `
     <nav class="navbar container">
         <ul class="navbar__item">
             <li class="navbar__item">
-                <a class="navbar__link" href="/TP-FSN-Grupo03/pages/shop.html">SHOP</a></li>
+                <a class="navbar__link" href="/pages/shop.html">SHOP</a></li>
             <li class="navbar__item">
-                <a class="navbar__link" href="/TP-FSN-Grupo03/pages/admin/register.html">REGISTRARSE</a></li>
+                <a class="navbar__link" href="/pages/Admin/register.html">REGISTRARSE</a></li>
             <li class="navbar__item">
-                <a class="navbar__link" href="/TP-FSN-Grupo03/pages/admin/login.html">LOGIN</a></li>
+                <a class="navbar__link" href="/pages/Admin/login.html">LOGIN</a></li>
             <li class="navbar__item">
-                <a class="navbar__link" href="/TP-FSN-Grupo03/pages/contact.html">CONTACTO</a></li>
+                <a class="navbar__link" href="/pages/contact.html">CONTACTO</a></li>
         </ul>
 
         <picture>
