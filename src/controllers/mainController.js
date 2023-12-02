10ca41abb17437007bfcc52bdb238574753a3741
Flavio <faltamirano@gruposis.com.ar>
2023-12-02 15:32:33 -0300

    Challenger5 - arreglo de selccion licencia y categoria

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,18 @@
 const fs = require ("fs")
 const licencesJSON = JSON.parse(fs.readFileSync("./data/licence.json","utf-8"))
-const lanzamientosJSON = JSON.parse(fs.readFileSync("./data/lanzamiento.json","utf-8"))
+const productosJSON = JSON.parse(fs.readFileSync("./data/producto.json","utf-8"))
+const sliderLanzamientosJSON = JSON.parse(fs.readFileSync("./data/sliderLanzamiento.json","utf-8"))
 
 const mainControllers = {
     home: (req, res) => {
-        res.render("home", {licences:licencesJSON, sliderItems:lanzamientosJSON, sliderTitulo: "Ultimos Lanzamientos"})
+        let lanzamientos = sliderLanzamientosJSON.map(e => e.product_id)
+        let sliderItems = []
+        productosJSON.forEach(producto => {
+            if (lanzamientos.includes(producto.product_id)) {
+                sliderItems.push(producto)
+            }
+        });
+        res.render("home", {licences:licencesJSON, sliderItems:sliderItems, sliderTitulo: "Ultimos Lanzamientos"})
     },
     contact: (req, res) => res.render("contact"),
     //about: (req, res) => res.send("Route for About View"),
--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,23 +1,47 @@
 const fs = require ("fs")
 const productosJSON = JSON.parse(fs.readFileSync("./data/producto.json","utf-8"))
-const relacionadosJSON = JSON.parse(fs.readFileSync("./data/relacionado.json","utf-8"))
+const sliderRelacionadosJSON = JSON.parse(fs.readFileSync("./data/sliderRelacionado.json","utf-8"))
 const shopControllers = {
     shop: (req, res) => {
-        res.render("shop", {productos:productosJSON})
+       let filtroCategoria = req.query.Cat 
+        let filtroLicencia = req.query.Lic
+        filtroCategoria = filtroCategoria==undefined ? "" : filtroCategoria;
+        filtroLicencia = filtroLicencia==undefined ? "" : filtroLicencia;
+       
+        if (filtroCategoria == "" && filtroLicencia == "") {
+            res.render("shop", {productos:productosJSON})
+        }
+        else {
+            let productos = []
+            productosJSON.forEach(producto => {
+                if ((filtroCategoria=="" || producto.category_name == filtroCategoria) && 
+                    (filtroLicencia=="" || producto.licence_name == filtroLicencia)) {
+                    productos.push(producto)
+                }
+            })
+                res.render("shop",  {productos:productos})
+        }
+      
     },
     item: (req, res) => {
         let productoBuscado = ""
-        console.log(req.params.id)
+        let productosRelacionados = sliderRelacionadosJSON.map(e => e.product_id)
+        let sliderItems = []
         productosJSON.forEach(producto => {
             if (producto.product_id == req.params.id) {
                 productoBuscado=producto;
             }
+            if (productosRelacionados.includes(producto.product_id)) {
+                sliderItems.push(producto)
+            }
         })
+     
         if (productoBuscado != "") {
-            res.render("item", {producto:productoBuscado, sliderItems:relacionadosJSON, sliderTitulo: "PRODUCTOS RELACIONADOS"})
+            res.render("item", {producto:productoBuscado, sliderItems:sliderItems, sliderTitulo: "PRODUCTOS RELACIONADOS"})
         }
     },
-     about: (req, res) => res.send("Route for About View"),
+    
+    about: (req, res) => res.send("Route for About View"),
     faqs: (req, res) => res.send("Route for Faqs View")
 }
 
