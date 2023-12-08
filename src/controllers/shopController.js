e58d28112c83f4937f879da0b59519e96f21751c
Cande Di Genova <candedigenova@gmail.com>
2023-12-08 11:39:19 -0300

    Agregue rutas y controladores de carrito cumpliendo con mision 4

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -1,24 +1,43 @@
-const fs = require ("fs")
-const productosJSON = JSON.parse(fs.readFileSync("./data/producto.json","utf-8"))
-const relacionadosJSON = JSON.parse(fs.readFileSync("./data/relacionado.json","utf-8"))
+const fs = require("fs");
+const productosJSON = JSON.parse(
+  fs.readFileSync("./data/producto.json", "utf-8")
+);
+const relacionadosJSON = JSON.parse(
+  fs.readFileSync("./data/relacionado.json", "utf-8")
+);
 const shopControllers = {
-    shop: (req, res) => {
-        res.render("shop", {productos:productosJSON})
-    },
-    item: (req, res) => {
-        let productoBuscado = ""
-        console.log(req.params.id)
-        productosJSON.forEach(producto => {
-            if (producto.product_id == req.params.id) {
-                productoBuscado=producto;
-            }
-        })
-        if (productoBuscado != "") {
-            res.render("item", {producto:productoBuscado, sliderItems:relacionadosJSON, sliderTitulo: "PRODUCTOS RELACIONADOS"})
-        }
-    },
-     about: (req, res) => res.send("Route for About View"),
-    faqs: (req, res) => res.send("Route for Faqs View")
-}
+  shop: (req, res) => {
+    res.render("shop", { productos: productosJSON });
+  },
+  item: (req, res) => {
+    let productoBuscado = "";
+    console.log(req.params.id);
+    productosJSON.forEach((producto) => {
+      if (producto.product_id == req.params.id) {
+        productoBuscado = producto;
+      }
+    });
+    if (productoBuscado != "") {
+      res.render("item", {
+        producto: productoBuscado,
+        sliderItems: relacionadosJSON,
+        sliderTitulo: "PRODUCTOS RELACIONADOS",
+      });
+    }
+  },
+  cart: (req, res) => {
+    res.render("carrito", {
+      title: "Carrito",
+      productos: productosJSON,
+    });
+  },
+  addToCart: (req, res) => {
+    const productId = req.body.productId;
+    res.send(`Producto ${productId} agregado a tu carrito con éxito`);
+  },
+
+  about: (req, res) => res.send("Route for About View"),
+  faqs: (req, res) => res.send("Route for Faqs View"),
+};
 
 module.exports = shopControllers;
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const shopControllers = require('../controllers/shopController')
+const shopControllers = require("../controllers/shopController");
 
-router.get("/", shopControllers.shop)
-router.get("/item/:id", shopControllers.item)
-router.post("/item/:id/add", (req, res) => res.send(`Route Item  ${req.params.id} Shop Add View`))
-router.get("/cart", (req, res) => res.send("Get - Route for Shop Cart View"))
-router.post("/cart", (req, res) => res.send("Post - Route for Shop Cart View"))
+router.get("/", shopControllers.shop);
+router.get("/item/:id", shopControllers.item);
+router.post("/item/:id/add", (req, res) =>
+  res.send(`Route Item  ${req.params.id} Shop Add View`)
+);
+router.get("/cart", shopControllers.cart);
+router.post("/cart", shopControllers.addToCart);
 
 module.exports = router;
