ae61483368f7e664b0a73a40bcd2c86b9676e5e7
Flavio <faltamirano@gruposis.com.ar>
2023-12-08 13:56:24 -0300

    Cambios en SHop - filtros de aside y ordenamiento

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -3,27 +3,52 @@ const productosJSON = JSON.parse(fs.readFileSync("./data/producto.json","utf-8")
 
 const shopControllers = {
     shop: (req, res) => {
-       let filtroCategoria = req.query.Cat 
+      
+        let filtroCategoria = req.query.Cat 
         let filtroLicencia = req.query.Lic
+        let filtroOrden = req.query.Ord
+
+        filtroOrden = filtroOrden==undefined ? "az" : filtroOrden;
         filtroCategoria = filtroCategoria==undefined ? "" : filtroCategoria;
         filtroLicencia = filtroLicencia==undefined ? "" : filtroLicencia;
-        let filtro = {categoria: filtroCategoria, licencia: filtroLicencia}
-        
+      
+        let filtro = {categoria: filtroCategoria, licencia: filtroLicencia, orden: filtroOrden}
+   
+        let productos = []
         if (filtroCategoria == "" && filtroLicencia == "") {
-            res.render("shop", {title:'Shop | FunkoShop', productos:productosJSON, filtro:filtro})
+           productos = productosJSON
         }
         else {
-            let productos = []
             productosJSON.forEach(producto => {
                 if ((filtroCategoria=="" || producto.category_name == filtroCategoria) && 
                     (filtroLicencia=="" || producto.licence_name == filtroLicencia)) {
                     productos.push(producto)
                 }
             })
-
-                res.render("shop",  {title:'Shop | FunkoShop', productos:productos, filtro:filtro})
+         }
+        
+        let productosOrdenados = []
+        if (filtroOrden == "az") {
+            productosOrdenados = productos.sort(function (a, b){
+                    return a.product_name.localeCompare(b.product_name, 'es', { numeric: true })
+                });
         }
-      
+        if (filtroOrden == "za") {
+            productosOrdenados = productos.sort(function (b, a){
+                    return a.product_name.localeCompare(b.product_name, 'es', { numeric: true })
+                });
+        }
+        if (filtroOrden == "asc") {
+            productosOrdenados = productos.sort(((a, b) => a.product_price - b.product_price))     
+        }
+        if (filtroOrden == "desc") {
+            productosOrdenados = productos.sort(((a, b) => b.product_price - a.product_price))     
+        }
+
+        console.log("ordenados: " + productosOrdenados)
+
+
+        res.render("shop",  {title:'Shop | FunkoShop', productos:productosOrdenados, filtro:filtro})
     },
     item: (req, res) => {
         let productoBuscado = ""
