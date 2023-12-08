ab7c31c20a932bd9f29f602cbffbd0a808596ded
Cande <133878782+candedg@users.noreply.github.com>
2023-12-08 14:30:01 -0300

    Update shopRoutes.js
    Rutas carrito

--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -5,7 +5,7 @@ const shopControllers = require('../controllers/shopController')
 router.get("/", shopControllers.shop)
 router.get("/item/:id", shopControllers.item)
 router.post("/item/:id/add", (req, res) => res.send(`Route Item  ${req.params.id} Shop Add View`))
-router.get("/cart", (req, res) => res.send("Get - Route for Shop Cart View"))
-router.post("/cart", (req, res) => res.send("Post - Route for Shop Cart View"))
+router.get("/cart", shopControllers.cart);
+router.post("/cart", shopControllers.addToCart);
 
 module.exports = router;
